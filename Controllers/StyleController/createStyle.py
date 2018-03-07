529418e8025e01b9f9029897655ead5fdfae85cb
Annabelle Tang <tangannabelle1@gmail.com>
2018-03-06 16:28:35 -0800

    Create createStyle.py
    Relocating the function of "create style" from views.py to createStyle.py.

---
+++ Controllers/StyleController/createStyle.py
@@ -0,0 +1,17 @@
+from . import *
+
+# Add a style
+@app.route('/styles/new', methods=['GET', 'POST'])
+@login_required
+def newStyle():
+	if request.method == 'POST':
+		newStyle = Styles(
+			type = request.form['type'],
+			user_id = login_session['user_id'])
+		print(newStyle)
+		session.add(newStyle)
+		flash('You have successfully added %s as a new style!' % newStyle.type)
+        session.commit()
+		return redirect(url_for('index.html'))
+	else:
+		return render_template('newStyle.html')
