af0d8dfd31c78de0811ab9c3363192b16658286d
Annabelle Tang <tangannabelle1@gmail.com>
2018-03-06 18:28:38 -0800

    Create styleTypeBasedInfluencer.py
    Relocated from views.py.

---
+++ Controllers/InfluencerController/styleTypeBasedInfluencer.py
@@ -0,0 +1,9 @@
+from . import *
+
+# Influencer based on style type json file
+@app.route('/styleInfluencers/<string:styles_type>/<string:influencer_name>/JSON')
+@login_required
+def influencerStylesJSON():
+	styles = session.query(Styles).filter_by(type=styles_type).one()
+	influencer = session.query(Influencers).filter_by(blog_name=influencer_blogName, type=styles_type).one()
+	return jsonify(influencers=[influencers.serialize])
