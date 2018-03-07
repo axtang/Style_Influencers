72d741cdbb094c6f203e066b2244cd940bc6064e
Annabelle Tang <tangannabelle1@gmail.com>
2018-03-06 16:30:47 -0800

    Create styleInfluencers.py
    Relocating the function that generates a json file containing both styles and influencers.

---
+++ Controllers/StyleController/styleInfluencers.py
@@ -0,0 +1,12 @@
+from . import *
+
+# Styles and influencers json file
+@app.route('/styleInfluencers/JSON')
+@login_required
+def allStyleInfluencersJSON():
+	styles = session.query(Styles).all()
+	styles_dict = [s.serializes for s in styles]
+	for s in range(len(styles_dict)):
+		categories = [c.serialize for c in session.query(Type).filter_by(styles_id=styles_dict[c]["id"]).all()]
+		if categories:
+			styles_dict[c]["type"] = categories
