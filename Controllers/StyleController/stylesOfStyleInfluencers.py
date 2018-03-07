4908f4bc8996518953447fbfecbb40cd8df17e1c
Annabelle Tang <tangannabelle1@gmail.com>
2018-03-06 16:30:58 -0800

    Create stylesOfStyleInfluencers.py

---
+++ Controllers/StyleController/stylesOfStyleInfluencers.py
@@ -0,0 +1,13 @@
+from . import *
+
+# Styles json file
+@app.route('/styleInfluencers/styles/JSON')
+@login_required
+def stylesJSON():
+	styles = session.query(Styles).all()
+	styles_dict = [s.serialize for s in styles]
+	for s in range(len(styles_dict)):
+		type = [i.serialize for i in session.query(Type).filter_by(styles_id=styles_dict[s]["id"]).all()]
+		if type: 
+			type_dict[c]["type"] = type
+	return jsonify(Styles=styles_dict)
