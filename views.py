52dfc9f6d3f054e69f54f50e9c3c62a4fe641696
Annabelle Tang <tangannabelle1@gmail.com>
2018-03-06 16:09:30 -0800

    Update views.py
    Deleting and relocating anti-forgery state token to login.py.

--- views.py
+++ views.py
@@ -35,30 +35,6 @@ session = DBSession()
 
 APPLICATION_NAME = "Style-Influencers Application"
 
-
-# anti-forgery state token
-@app.route('/login')
-def showLogin():
-    state = ''.join(
-        random.choice(string.ascii_uppercase + string.digits)
-        for x in range(32))login_session['state'] = state
-    return render_template('login_template')
-
-
-# take in a google one-time use code
-# exchange this auth-code for an access token
-
-
-# Add Flask login_decorator
-def login_required(f):
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        if g.user is None:
-            return redirect(url_for('login', next=request.url))
-        return f(*args, **kwargs)
-    return decorated_function
-
-
 # Home page
 @app.route('/')
 @app.route('/home')
