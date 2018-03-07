bad4cbbf1590eeb894882a4ef339bb599a970229
Annabelle Tang <tangannabelle1@gmail.com>
2018-03-06 18:27:00 -0800

    Create styleType.py

---
+++ Controllers/StyleController/styleType.py
@@ -0,0 +1,9 @@
+from . import *
+
+# Style type json file
+@app.route('/styleInfluencers/<string:styles_type>/influencers/JSON')
+@login_required
+def stylesInfluencersJSON(styles_type):
+	style = session.query(Type).filter_by(type=styles_type).one()
+	influencers = session.query(Influencers).filter_by(styles=styles).all()
+	return jsonify(influencers=[i.serialize for i in influencers])
