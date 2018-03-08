5ed7b34be47a82e00e02fbe5db3f41a00454db9a
Annabelle Tang <tangannabelle1@gmail.com>
2018-03-07 19:23:16 -0800

    Update views.py
    Changing port to 80 and register blueprints.

--- views.py
+++ views.py
@@ -8,7 +8,9 @@ APPLICATION_NAME = "Style-Influencers Application"
 
 
 if __name__ == '__main__':
-    app.secret_key = 'super_secret_key'
-	app.debug = True
+    app.register_blueprint(home)
+    app.register_blueprint(style)
+    app.register_blueprint(influencer)
+    app.register_blueprint(auth)
 	# app.config['SECRET_KEY'] = ''.join(random.choice(string.ascii_uppercase + string.digits) for x xrange(32))
-	app.run(host='0.0.0.0', port=5000)
+	app.run(host='0.0.0.0', port=80, debug=True)
