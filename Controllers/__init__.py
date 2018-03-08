b4bcdf371d975da40678c5888807f30a8080745b
Annabelle Tang <tangannabelle1@gmail.com>
2018-03-07 17:22:49 -0800

    Create __init__.py
    Creating __init__.py for the Controllers file.

---
+++ Controllers/__init__.py
@@ -0,0 +1,25 @@
+# Import HomeController
+from HomeController.homePage import *
+
+# Import GoogleOAuthController
+from GoogleOAuthController.gconnect import *
+from GoogleOAuthController.gdisconnect import *
+from GoogleOAuthController.login import *
+
+# Import StyleController
+from StyleController.allStyles import *
+from StyleController.createStyle import *
+from StyleController.deleteStyle import *
+from StyleController.editStyle import *
+from StyleController.styleInfluencers import *
+from StyleController.stylesOfStyleInfluencers import *
+from StyleController.styleType import *
+
+# Import InfluencerController
+from InfluencerController.allInfluencers import *
+from InfluencerController.createInfluencers import *
+from InfluencerController.deleteInfluencers import *
+from InfluencerController.editInfluencers import *
+from InfluencerController.SIBasedInfluencers import *
+from InfluencerController.styleBasedInfluencer import *
+from InfluencerController.styleTypeBasedInfluencer import *
