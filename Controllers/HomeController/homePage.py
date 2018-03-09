3bb35406f46186d9a6ffd16c7bac6a2739b408aa
Annabelle Tang <tangannabelle1@gmail.com>
2018-03-09 08:55:39 -0800

    Update homePage.py
    Change '@app.route' to '@home.route'.

--- Controllers/HomeController/homePage.py
+++ Controllers/HomeController/homePage.py
@@ -1,9 +1,9 @@
 from . import *
 
 # Home page
-@app.route('/')
-@app.route('/home')
+@home.route('/')
+@home.route('/home')
 def Home():
 	styles = session.query(Styles).order_by(asc(Styles.type))
 	influencers = session.query(Influencers).order_by(asc(Influencers.name))
-	return render_template('index.html')
+	return render_template('index.html', styles=Styles, influencers=Influencers)
