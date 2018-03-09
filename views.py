56c623143bc50a4de33d96d1c81d215e028e906a
Annabelle Tang <tangannabelle1@gmail.com>
2018-03-09 08:51:39 -0800

    Update views.py
    Changing 'http://localhost:5000/home' to 'http://localhost'.

--- views.py
+++ views.py
@@ -1,6 +1,5 @@
-
 from flask import Flask
-from Controllers import *
+from controllers import *
 
 app = Flask(__name__)
 
@@ -12,5 +11,4 @@ if __name__ == '__main__':
     app.register_blueprint(style)
     app.register_blueprint(influencer)
     app.register_blueprint(auth)
-	# app.config['SECRET_KEY'] = ''.join(random.choice(string.ascii_uppercase + string.digits) for x xrange(32))
-	app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
